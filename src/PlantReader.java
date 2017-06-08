c927c8a3bae7ea64ed81c6eb473e6df1c6edfd67
unknown <salilthip.p@ku.th>
2017-06-08 15:53:30 +0900

    Clean for rewrite state and sequence diagram together

--- src/PlantReader.java
+++ src/PlantReader.java
@@ -78,134 +78,6 @@ public class PlantReader {
     	
 	}
 	
-//	  public static String translateToChannel(ArrayList<String> result){
-//	    	String res = "channel ";
-//	    	int msg = 0;
-//	    	System.out.println("ListData :"+traceData);
-//	    	for (int i = 0; i < result.size(); i++) {
-//	    		for (int j = 0; j < traceData.size(); j++) {
-//	    			if(traceData.get(j).contains(result.get(i))) res+=traceData.get(j).get(0).values()+",";
-//	    			
-//				}
-//			}
-//	    	return res;
-//	    }
-//	    
-	    
-	    
-//	    public static String translateToState(String nameState,ArrayList<String> a){
-//	    	String res = "";
-//	    	String status = nameState;
-//	    	for (int i = 0; i < traceData.size(); i++) {
-//	    		for (int j = 0; j < traceData.get(i).size(); j++) {
-//	    			Map map = traceData.get(i).get(j);
-//	    			if(!map.containsKey("Message")){
-//	    				String[] arr = map.toString().split("=");
-//	    				String[] state = arr[0].split(", ");
-//	    				String start = "",re = "",po = "",dest = "";
-//	    				start= state[0].replace("{[", "");
-//	    				re= arr[1].toString().replace("}", "");
-//	    				po= state[1];
-//	    				dest= state[2].replace("]", "");
-//	    				res += status+" = ";
-//	    				status = dest;
-//	    				if(!re.equals("NaN")){
-//	    				res += readAction(re);
-//	    				}
-//	    				res += dest+"\n";
-//
-//	    			}
-//				}
-//				
-//			}
-//	    	return res;
-//	    }
-//	    
-	    public static String readAction(String message){
-	    	String rs="";
-	    	System.out.println("Message :"+message);
-	    	ArrayList<String> a = getArrayFromMsg(message);
-	    	for (int i = 0; i < a.size(); i++) {
-				if (a.get(i).length()!=0) {
-					rs+=a.get(i)+" -> ";
-				}
-				
-			}
-	    	return rs;
-	    }
-	
-	 public static ArrayList<String> getArrayFromMsg(String msg){
-	    	ArrayList<String> a;
-	    	if(msg.contains(":")){
-		    	String[] arr = msg.split(":");
-		    	System.out.println("T"+arr);
-		    	if(arr[1].contains("/"))
-		    	a = new  ArrayList<>(Arrays.asList(arr[1].split("/")));
-		    	else{ 
-		    		a = new ArrayList<>();
-		    		a.add(arr[1]);
-		    		a.add("");
-		    	}
-	    	}else if(msg.contains("/") ){
-				a = new ArrayList<>(Arrays.asList(msg.split("/")));
-			}else {
-				a = new ArrayList<>();
-				a.add(msg);
-				a.add("");
-				}
-	    	return a;
-	    }
-	
-    public static void translateMsg(String state,String op,String msg,int i,ArrayList<String> result){
-    	String res="";
-    	if(result.get(i+2).equals(":")){
-    		ArrayList<String> t = getArrayFromMsg(msg);
-    		System.out.println("MM :"+msg);
-    		System.out.println("T :"+t);
-    		for (int j = 0; j < t.size(); j++) {
-    			System.out.println("j :"+j+",t :"+t.get(j));
-    			if(t.get(j).length()!=0)
-        		if(j%2==1)
-            		res += "/"+t.get(j)+"s";
-        		else if(j==0)
-            		res += t.get(j)+"r";
-        		else res += "/"+t.get(j)+"r";
-			}
-
-//    		isSend = !isSend;
-    		result.add(i+3, res);
-    		System.out.println("Res :"+res);
-//    		updateMessageToArrayList(result.get(i+3));
-    	}
-    }
-    
-//    public static void updateMessageToArrayList(String msg){
-//    	Map<String, String> map;
-//    	map = new HashMap<>();
-//    	if(msg.charAt(0)=='/' ||msg.charAt(msg.length()-1)=='/') msg = msg.replace("/", "");
-//    	map.put("Message", msg);
-//    	LinkedList<Map> linkMap = new LinkedList<>();
-//    	linkMap.add(map);
-//    	traceData.add(linkMap);
-//    }
-//    
-//    public static void updateToArrayList(String left,String med,String right,String msg){
-//    	LinkedList<String> linkList = new LinkedList<>();
-//		Map<LinkedList<String>, String> map;
-//		map = new HashMap<>();
-//    	linkList.add(left);
-//		linkList.add(med);
-//		linkList.add(right);
-//		if(msg.equals("->")||msg.equals("-->")||msg.equals("<-")||msg.equals("<--")||msg.equals(":"))
-//			msg = "NaN";
-//		if(msg.charAt(0)=='/' ||msg.charAt(msg.length()-1)=='/') msg = msg.replace("/", "");
-//		map.put(linkList, msg);
-//		System.out.println("Message :"+msg);
-//		LinkedList<Map> linkMap = new LinkedList<>();
-//		linkMap.add(map);
-//		traceData.add(linkMap);
-//    }
-    
 
 	
 	 public static void translateToDiagram(Map<String,ArrayList<String>> map){
@@ -235,9 +107,7 @@ public class PlantReader {
 		for (int j = 1; j < res.size()-1; j++) {
 			String tmp = res.get(j);
 			LinkedList<String> ll = new LinkedList<>();
-			System.out.println("i :"+tmp);
-			System.out.println("i-1 :"+res.get(j-1));
-			System.out.println("i+1 :"+res.get(j+1));
+
 			if(tmp.contains("<")){
 	    		ll.add(res.get(j+1));
 	    		ll.add(tmp.replace("<", "")+">");
