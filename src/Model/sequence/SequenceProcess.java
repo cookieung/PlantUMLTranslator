e04cf3e5de708beddd0c39e54b0792cf3d902e61
Salilthip Phuklang <ung_1002@hotmail.com>
2017-07-25 03:10:27 +0900

    Freeze for continue

--- src/Model/sequence/SequenceProcess.java
+++ src/Model/sequence/SequenceProcess.java
@@ -112,16 +112,12 @@ public class SequenceProcess extends ProcessList {
 		 for (int i = 0; i < l.size(); i++) {
 			for (Entry<String, Map<String, Map<String, String>>> map : l.get(i).entrySet()) {
 				System.out.println("CV Frame :"+map);
+				System.out.println("Map get Key :"+map.getKey()+" = "+nameKey);
 				if(map.getKey().contains("alt") || map.getKey().contains("opt") || map.getKey().contains("loop")){
 					begin = map.getValue();
 					allFrameProcess.add(map.getValue());
 					nameKey = map.getKey();
-				}else if(map.getKey().equals("else")){
-					end = map.getValue();
-					allFrameProcess.add(map.getValue());
-					SequenceFrame frame = new SequenceFrame("F"+ ++counter,nameKey,begin,end);
-					frames.add(frame);
-				}else if(map.getKey().contains("end") && nameKey.contains("loop")) {
+				}else if(map.getKey().contains("end")) {
 					end = map.getValue();
 					allFrameProcess.add(map.getValue());
 					SequenceFrame frame = new SequenceFrame("F"+ ++counter,nameKey,begin,end);
